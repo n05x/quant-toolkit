711fe9ebaef98130d850e038ede56f94d97b67f6
three <three@turin>
2026-03-18 11:56:32 -0500

    added to init

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
+from .glm4_7 import Glm4_7Config
 from .glm5 import Glm5Config
 from .minimax_m25 import MinimaxM25Config
 from .qwen3_5_122b import Qwen35_122BConfig
@@ -5,6 +6,7 @@ from .qwen3_5_moe import Qwen35MoeConfig
 from .qwen3_5_moe_noshared import Qwen35MoeNoSharedConfig
 
 _CONFIGS = {
+    "glm4_7": Glm4_7Config,
     "glm5": Glm5Config,
     "minimax_m25": MinimaxM25Config,
     "qwen3_5_122b": Qwen35_122BConfig,
